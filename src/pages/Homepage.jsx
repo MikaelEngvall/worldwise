bf24a62adc37e4bdf4a8306f79b074357a92ec7a
Mikael Engvall <mikael.engvall.me@gmail.com>
2025-01-12 12:07:10 +0100

    Add new components and styles for Logo, Message, Spinner, Button, CityList, and Sidebar; implement ProtectedRoute and update PageNotFound

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,25 @@
-import PageNav from "../components/PageNav"
-function Homepage() {
-    return (
-        <div>
-            <PageNav />
-            <h1>WorldWise</h1> 
-        </div>
-    )
+import styles from "./Homepage.module.css";
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+export default function Homepage() {
+  return (
+    <main className={styles.homepage}>
+      <PageNav />
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+        <Link to="/login" className="cta">
+          Start traking now
+        </Link>
+      </section>
+    </main>
+  );
 }
-
-export default Homepage
